bf3055c517f131eaf384b3a5cab531943fd041f8
Angga Sya'ban Rahid <50766782+anggasybnrhd@users.noreply.github.com>
2023-11-11 21:27:40 +0700

    peruntukan soal no 3

---
+++ jawaban.py
@@ -0,0 +1,30 @@
+
+# note: tipedata di Python bahasa pemrograman yang dinamis dan juga tidak perlu deklarasi tipe secara eksplisit. cukup sebut aja.
+#Penting untuk dicatat bahwa Python adalah bahasa yang mendukung dynamic typing, yang berarti tipe data variabel dapat berubah selama runtime. Sebagai contoh:
+
+# Integer (int): Tipe data untuk menyimpan bilangan bulat.
+angka_bulat = 10
+
+# Float (float): Tipe data untuk menyimpan bilangan pecahan.
+angka_pecahan = 3.14
+
+#String (str): Tipe data untuk menyimpan teks.
+teks = "Hello, World!"
+
+#Boolean (bool): Tipe data untuk menyimpan nilai kebenaran (True atau False).
+benar = True
+salah = False
+
+#List (list): Tipe data untuk menyimpan sejumlah nilai berurutan. List dapat berisi tipe data apapun, termasuk campuran.
+daftar_angka = [1, 2, 3, 4, 5]
+daftar_bercampur = [1, "dua", 3.0, True]
+
+#Tuple (tuple): Mirip dengan list, tetapi bersifat tidak dapat diubah (immutable).
+tupel_angka = (1, 2, 3)
+
+#Set (set): Tipe data untuk menyimpan himpunan nilai unik tanpa urutan.
+himpunan_angka = {1, 2, 3, 4, 5}
+
+#Dictionary (dict): Tipe data untuk menyimpan pasangan kunci-nilai (key-value pairs).
+data = {"nama": "John", "umur": 25, "pekerjaan": "programmer"}
+
